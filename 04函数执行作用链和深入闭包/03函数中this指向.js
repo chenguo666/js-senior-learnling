e91387a91466a9c6729a8bb805201e242f6c7f7a
chenguo6666 <cg1305378470@163.com>
2022-01-14 21:33:27 +0800

    this指向 new 显示绑定

--- 04函数执行作用链和深入闭包/03函数中this指向.js
+++ 04函数执行作用链和深入闭包/03函数中this指向.js
@@ -53,5 +53,36 @@ function e(){
 var g = e()
 g()
 // 2.隐式绑定
+// 通过某个对象进行调用
+// xxx.xx()
+// object对象会被js引擎绑定到fn函数中的this里面
+var obj1={
+    name:'obj1',
+    foo:function () {
+        console.log(this);
+    }
+}
+var obj2 = {
+    name:'obj2',
+    bar:obj1.foo
+}
+obj2.bar()
 // 3.显示绑定
+// 前提 必须在调用的对象内部有一个函数的引用
+function foo1() {
+    console.log('函数被调用',this);
+}
+var obg3={
+    name:'obj3'
+}
+// this的绑定不同
+foo1()
+//  call apply可以指定this绑定对象
+foo1.call(obg3,1,1,1) //后面传参数 
+foo1.apply(obg3,[1,1,1]) // 后面传数组 
+// bind
 // 4.new绑定
+// 创建一个全新的对象
+// 这个新对象会被执行prototype
+// 这个新对象会绑定到函数调用的this上
+// 如果函数没有返回其他对象 表达式会返回这个新对象

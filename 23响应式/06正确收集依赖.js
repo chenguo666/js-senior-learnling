264c1bcc908bd18fb1adb9c9325952035039c266
fruitchan <1305378470@qq.com>
2022-03-02 16:52:27 +0800

    响应式优化

---
+++ 23响应式/06正确收集依赖.js
@@ -0,0 +1,75 @@
+// 封装一个响应式函数
+const obj = {
+  name: "why",
+  age: 18,
+};
+class Depend {
+  constructor() {
+    this.reactiveFns = [];
+  }
+  addDepend(reactiveFn) {
+    this.reactiveFns.push(reactiveFn);
+  }
+  notify() {
+    this.reactiveFns.forEach((fn) => {
+      fn();
+    });
+  }
+}
+let activeReactiveFn = null;
+function watchFn(fn) {
+  activeReactiveFn = fn;
+  fn();
+}
+// 封装一个获取depend的函数
+const targetMap = new WeakMap();
+
+function getDepend(target, key) {
+  let map = targetMap.get(target);
+  if (!map) {
+    map = new Map();
+    targetMap.set(target, map);
+  }
+  let depend = map.get(key);
+  if (!depend) {
+    depend = new Depend();
+    map.set(key, depend);
+  }
+  return depend;
+}
+const objProxy = new Proxy(obj, {
+  get: function (target, key, receiver) {
+    console.log("get", key);
+    const depend = getDepend(target, key);
+    depend.addDepend(activeReactiveFn);
+    return Reflect.get(target, key, receiver);
+  },
+  set: function (target, key, newValue, receiver) {
+    console.log("set", key);
+    Reflect.set(target, key, newValue, receiver);
+    const depend = getDepend(target, key);
+    depend.notify();
+  },
+});
+// 需要响应式的函数
+watchFn(function () {
+  const newName = objProxy.name;
+  console.log("你好", newName);
+  console.log(objProxy.name);
+});
+// 不需要响应式的函数
+function bar() {
+  console.log("不需要响应式！", objProxy.name);
+}
+objProxy.name = "safd";
+objProxy.name = "adsfaaa";
+objProxy.name = "adsfaaa45";
+objProxy.age = 12;
+
+// const info = {
+//   address: "wfs",
+// };
+// watchFn(function () {
+//   console.log(info.address, "监听address");
+// });
+// info.address = "asdaaa";
